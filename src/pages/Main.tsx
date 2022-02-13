518b8ea222349ab886a0a648c961b70ba857be58
Mark Dewey <mark.t.dewey@gmail.com>
2022-02-13 16:23:11 -0500

    (attempt) new CD

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Main = () => {
   return (
     <div className="App">
-      <section className="search">search!</section>
+      <section className="search">search this!</section>
       <section className="creature-display">Checkout these stats</section>
       <section className="creature-tools">Modify here!</section>
     </div>
