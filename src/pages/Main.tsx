b2a914dd283eb1f853143f24873620068be57dd2
Mark Dewey <mark.t.dewey@gmail.com>
2022-02-13 11:00:03 -0500

    (chore) project init

---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="App">
+      <section className="search">search!</section>
+      <section className="creature-display">Checkout these stats</section>
+      <section className="creature-tools">Modify here!</section>
+    </div>
+  );
+}
+
+export default Main;
